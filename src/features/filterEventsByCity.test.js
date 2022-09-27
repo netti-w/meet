fe586d280a3c2df09807fb2aff2d8c10b46f0be1
netti-w <106514885+netti-w@users.noreply.github.com>
2022-09-27 21:42:18 +0200

    Delete space

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -41,7 +41,6 @@ defineFeature(feature, test => {
     });
   });
 
-
   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
     let AppWrapper;
     given('user was typing “Berlin” in the city textbox', async () => {
