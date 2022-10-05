0e59739e1d8c56ca7cb4b9e1c1054c3103bb8714
netti-w <106514885+netti-w@users.noreply.github.com>
2022-10-05 13:40:07 +0200

    Add scatter chart for key feature 5

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { getEvents, extractLocations } from './api';
+// import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 class App extends Component {
@@ -18,7 +20,7 @@ class App extends Component {
       numberOfEvents: 10,
       selectedLocation: 'all',
       warningText: '',
-      showWelcomeScreen: undefined
+      // showWelcomeScreen: undefined
     }
   }
 
@@ -44,21 +46,44 @@ class App extends Component {
     });
   }
 
-  async componentDidMount() {
-    this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-      true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
+  componentDidMount() {
+    {
+      this.mounted = true;
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events,
+            locations: extractLocations(events)
+          });
         }
       });
     };
+
+    // async componentDidMount() {
+    //   this.mounted = true;
+    //   const accessToken = localStorage.getItem('access_token');
+    //   const isTokenValid = (await checkToken(accessToken)).error ? false :
+    //     true;
+    //   const searchParams = new URLSearchParams(window.location.search);
+    //   const code = searchParams.get("code");
+    //   this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    //   if ((code || isTokenValid) && this.mounted) {
+    //     getEvents().then((events) => {
+    //       if (this.mounted) {
+    //         this.setState({ events, locations: extractLocations(events) });
+    //       }
+    //     });
+    //   };
 
     if (!navigator.onLine) {
       this.setState({
@@ -76,17 +101,33 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.showWelcomeScreen === undefined) return <div
-      className="App" />
+    // if (this.state.showWelcomeScreen === undefined) return <div
+    //   className="App" />
 
     return (
       <div className="App">
         <WarningAlert text={this.state.warningText} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400} >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#ff7580" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => { getAccessToken() }} />
+        {/* <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} /> */}
       </div>
     );
   }
--- src/api.js
+++ src/api.js
@@ -47,7 +47,7 @@ const getToken = async (code) => {
 //   }
 // }
 
-export const checkToken = async (accessToken) => {
+const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
@@ -56,6 +56,16 @@ export const checkToken = async (accessToken) => {
 
   return result;
 };
+
+// export const checkToken = async (accessToken) => {
+//   const result = await fetch(
+//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+//   )
+//     .then((res) => res.json())
+//     .catch((error) => error.json());
+
+//   return result;
+// };
 
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
